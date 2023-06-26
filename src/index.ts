0f5778f19ce7bafcf51c5732df468668e649873b
TechvibesDev <jigbashio@gmail.com>
2023-06-26 20:47:27 +0100

    Modify type definations and package.json

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as usePayWithMonnifyPayment } from './monnify/payWithMonnify'
 export { default as MonnifyPaymentButton } from './monnify/button'
 export { default as PayWIthMonnifyPayment } from './monnify/payment'
+export { MonnifyProps } from './types/types'
